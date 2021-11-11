b7e00775b24968909f1d0b2e8db467ef6d700792
Jim Hessin <jhessin@gmail.com>
2021-11-10 22:58:15 -0700

    A multithreaded example

--- python/main.py
+++ python/main.py
@@ -1,32 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pipenv-shebang
 # This is a sample Python script.
 
 # Press Ctrl+Alt+R to execute it or replace it with your code. Press Double
 # Shift to search everywhere for classes, files, tool windows, actions,
 # and settings.
 
-def print_argument(func):
-    def wrapper(the_number):
-        print(f"Argument for {func.__name__} is {the_number}")
-        return func(the_number)
-    return wrapper
+import threading
+import time
 
 
-@print_argument
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    if name == '':
-        print("You didn't enter your name!")
-    else:
-        print("Hi there...")  # Press Ctrl+F8 to toggle the breakpoint.
-
-    for letter in name:
-        print(letter)
+# An I/O intensive calculation.
+# We simulate it with sleep.
+def heavy(n, my_id):
+    time.sleep(n)
+    print(my_id, "is done")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    your_name = input("Enter your name:")
-    print_hi(your_name)
+def threaded(n):
+    threads = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(n):
+        t = threading.Thread(target=heavy, args=(2, i,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+
+if __name__ == "__main__":
+    start = time.time()
+    threaded(80)
+    end = time.time()
+    print("Took: ", end - start)
